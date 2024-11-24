5ce5eb4b74a974b082bd4f2df19f2c0125d10d2d
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-24 12:13:06 +0500

    zustand userStore and socket.io connection establish front

--- frontend/src/store/userStore.js
+++ frontend/src/store/userStore.js
@@ -1,1 +1,44 @@
+import { create } from "zustand";
+import { io } from "socket.io-client";
 
+export const useUserStore = create((set) => ({
+  user: null,
+  socket: null,
+
+  // Set the username
+  setUser: (user) => set({ user }),
+
+  // Clear the user
+  clearUser: () => {
+    set({ user: null, socket: null });
+    if (useUserStore.getState().socket) {
+      useUserStore.getState().socket.disconnect();
+    }
+  },
+
+
+  connectSocket: () => {
+    const { user } = useUserStore.getState();
+
+    if (!user) {
+      console.error("Cannot connect socket without a username.");
+      return;
+    }
+
+    const socket = io("http://localhost:5000", {
+      query: { username: user }, 
+      withCredentials: true,   
+    });
+
+  
+    socket.on("connect", () => {
+      console.log("Connected to server with socket ID:", socket.id);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server.");
+    });
+
+    set({ socket });
+  },
+}));
