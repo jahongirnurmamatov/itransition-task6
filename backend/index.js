c534285da19c6077b4fcc782f058ac5cfcb1dda0
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-24 12:06:25 +0500

    backend socket.io setup

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from  'cors';
+import { app, io, server } from './lib/socket.io.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 
-const app = express();
+app.use(express.json());
+app.use(cors(
+    {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+        credentials: true,
+        
+    }
+));
 
+io.on("connection",(socket)=>{
+    console.log('A user connected: ',socket.id);
 
-app.listen(PORT, ()=>{
+    socket.on('disconnect', () => {
+        console.log('A user disconnected: ', socket.id);
+    });
+});
+
+server.listen(PORT, ()=>{
     console.log(`Server is running on PORT: ${PORT}`);
 })
