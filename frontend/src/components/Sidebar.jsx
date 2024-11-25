e6e195710a0f154b01476c7b0c0a7f117fe02e56
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-25 12:11:29 +0500

    schadcn integration

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const Sidebar = () => {
+  return (
+    <div className='w-full flex mt-16'>
+        <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default Sidebar
--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const Layout = ({ children }) => {
   return (
     <div className="bg-[#18191b] min-h-screen w-full">
       <Navbar/>
+      <Sidebar/>
       <Outlet />
     </div>
   );
